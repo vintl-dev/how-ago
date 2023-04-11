09e0fc61a228441c6838e77597c066e92032669e
Sasha Sorokin <10401817+Brawaru@users.noreply.github.com>
2023-04-11 18:20:53 +0200

    chore: disable vitest node loader

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,11 +3,6 @@ import { defineConfig } from 'vitest/config'
 import tsconfig from './tsconfig.tests.json'
 
 export default defineConfig({
-  test: {
-    deps: {
-      registerNodeLoader: true,
-    },
-  },
   esbuild: {
     tsconfigRaw: tsconfig as any,
   },
